8f857d38894fd6da1f446e2c9634f75f802069de
mrm <matt.rmoore@gmail.com>
2019-06-27 15:23:13 -0500

    FlickList2 complete, rename project folders, add ajaxson5 starter code

--- Course_LC101/09_SemanticHTML/FlickList2/js/flicklist.js
+++ Course_LC101/09_SemanticHTML/FlickList2/js/flicklist.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
   discoverMovies(render);
 });
@@ -14,7 +12,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "8635a9331ce7ad7cbe506004d34f79e9" // TODO put your api key here
 }
 
 
@@ -48,11 +46,21 @@ function discoverMovies(callback) {
 function searchMovies(searchTerm, callback) {
   console.log("searching for movies with '" + searchTerm + "' in their title...");
 
-  // TODO 9
+  $.ajax({
+    url: api.root + "/search/movie",
+    data: {
+      api_key: api.token,
+      query: searchTerm
+    },
+    success: function(response) {
+      model.browseItems = response.results;
+      callback();
+    }
+  });
+
+  // TODO 9 (COMPLETE)
   // implement this function as described in the comment above
   // you can use the body of discoverMovies as a jumping off point
-
-
 }
 
 
@@ -70,7 +78,8 @@ function render() {
     var title = $("<p></p>").text(movie.original_title);
     var itemView = $("<li></li>")
       .append(title)
-      // TODO 3
+      .attr("class", "item-watchlist");
+      // TODO 3 (COMPLETE)
       // give itemView a class attribute of "item-watchlist"
 
     $("#section-watchlist ul").append(itemView);
@@ -84,23 +93,32 @@ function render() {
       .click(function() {
         model.watchlistItems.push(movie);
         render();
-      });
-      // TODO 2
+      })
+      // TODO 2 (COMPLETE)
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
+      if (model.watchlistItems.indexOf(movie) >= 0) {
+        button.prop("disabled", true);
+      } else {
+        button.prop("disabled", false);
+      }
+
+      
 
 
-    // TODO 1
+    // TODO 1 (COMPLETE)
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
+    var movieOverview = $('<p></p>').text(movie.overview);
 
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(movieOverview)
       .append(button);
 
     // append the itemView to the list
