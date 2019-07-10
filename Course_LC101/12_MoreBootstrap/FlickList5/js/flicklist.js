583eca52260b129aafc8fb26b711d8f5d5b6498b
mrm <matt.rmoore@gmail.com>
2019-07-10 16:36:13 -0500

    FlickList5 complete except search function does not cooperate with carousel

--- Course_LC101/12_MoreBootstrap/FlickList5/js/flicklist.js
+++ Course_LC101/12_MoreBootstrap/FlickList5/js/flicklist.js
@@ -1,27 +1,18 @@
-
-
 var model = {
   watchlistItems: [],
-  browseItems: []
-
-  // TODO 
-  // add a property for the current active movie index
+  browseItems: [],
+  activeMovieIndex: 0   // TODO - add a property for the current active movie index
 }
-
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
-  /**
-   * Given a movie object, returns the url to its poster image
-   */
+  token: "8635a9331ce7ad7cbe506004d34f79e9", // TODO put your api key here
+  //Given a movie object, returns the url to its poster image
   posterUrl: function(movie) {
     var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
     return baseImageUrl + movie.poster_path; 
   }
 }
-
-
 
 /**
  * Makes an AJAX request to themoviedb.org, asking for some movies
@@ -31,7 +22,6 @@ var api = {
  */
 
 function discoverMovies(callback, keywords) {
-
   $.ajax({
     url: api.root + "/discover/movie",
     data: {
@@ -44,7 +34,6 @@ function discoverMovies(callback, keywords) {
     }
   });
 }
-
 
 /**
  * Makes an AJAX request to the /search/keywords endpoint of the API, using the 
@@ -62,17 +51,24 @@ function searchMovies(query, callback) {
       query: query
     },
     success: function(response) {
-      console.log(response);
-    
-      var keywordIDs = response.results.map(function(keywordObj) {
-        return keywordObj.id;
-      });
+      console.log("RESPONSE", response.results);
+      response.results.map(getMovieID);
+
+      var keywordIDs = response.results.map(getMovieID);
+      console.log(keywordIDs);
+
       var keywordsString = keywordIDs.join("|");
       console.log(keywordsString);
-      
+
       discoverMovies(callback, keywordsString);
+
     }
   });
+
+   function getMovieID(movie) {
+     console.log(movie.id);
+     return movie.id;
+  }
 }
 
 
@@ -123,27 +119,39 @@ function render() {
   });
 
   // render browse items
-  model.browseItems.forEach(function(movie) {
-    var title = $("<h4></h4>").text(movie.original_title);
-    var overview = $("<p></p>").text(movie.overview);
+  //model.browseItems.forEach(function(movie) {
+  var activeMovie = model.browseItems[model.activeMovieIndex];
+  //console.log("Active Title: ", activeMovie.original_title);
 
-    // button for adding to watchlist
-    var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .attr("class", "btn btn-primary")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      })
-      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+  $("#browse-info h4").text(activeMovie.original_title);
+  $("#browse-info p").text(activeMovie.overview);
 
-    var itemView = $("<li></li>")
-      .attr("class", "list-group-item")
-      .append( [title, overview, button] );
-      
-    // append the itemView to the list
-    $("#section-browse ul").append(itemView);
+  //allow the hard-coded button to add movies to watchlist
+  var button = $("#add-to-watchlist");
+  button.attr("class", "btn btn-primary");
+  button.unbind("click");
+  button.click(function() {
+    model.watchlistItems.push(activeMovie);
+    render();
   });
+  button.prop("disabled", model.watchlistItems.indexOf(activeMovie) !== -1);
+
+
+  // starter code provided
+  // fill carousel with posters
+  var posters = model.browseItems.map(function(movie) {
+    var poster = $("<img></img>")
+      .attr("src", api.posterUrl(movie))
+      .attr("class", "img-responsive");
+    
+    // TODO -return a list item with an img inside  
+    return $("<li></li>")
+      .attr("class", "item")
+      .append(poster);
+  });
+  $("#section-browse .carousel-inner").append(posters);
+  posters[model.activeMovieIndex].addClass("active");
+//  });
 }
 
 
